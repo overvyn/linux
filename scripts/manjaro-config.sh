1d17d27b9642242b517b42b6779b84fa8ea3d3a9
overvyn <overvyn@gmail.com>
2020-09-11 11:42:48 -0300

    check shell

--- scripts/manjaro-config.sh
+++ scripts/manjaro-config.sh
@@ -2,30 +2,34 @@
 # DEV PACKAGES #
 ################
 
-#ZSH
-sudo pacman -Syy zsh git curl
+if [ -d ~/.oh-my-zsh ] 
+then
+    # THEMES
+    cd ~/.oh-my-zsh/custom/themes
+    #git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+    git clone https://github.com/romkatv/powerlevel10k.git
 
-#ZSH OH-MY-ZSH
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    # PLUGINS
+    cd ~/.oh-my-zsh/plugins
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
-# THEMES
-cd ~/.oh-my-zsh/custom/themes
-#git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-git clone https://github.com/romkatv/powerlevel10k.git
+    ## EDITAR .zshrc
+    sed -i '/^plugins=(git)/c\plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' ~/.zshrc
+    sed -i '/^# ENABLE_CORRECTION="true"/c\ENABLE_CORRECTION="true"' ~/.zshrc
+    sed -i '/^ZSH_THEME="robbyrussell"/c\ZSH_THEME="powerlevel10k/powerlevel10k"' ~/.zshrc
 
-# PLUGINS
-cd ~/.oh-my-zsh/plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+else
+    #ZSH
+    sudo pacman --noconfirm -Syy zsh git curl
 
-## EDITAR .zshrc
-sed -i '/^plugins=(git)/c\plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' ~/.zshrc
-sed -i '/^# ENABLE_CORRECTION="true"/c\ENABLE_CORRECTION="true"' ~/.zshrc
-sed -i '/^ZSH_THEME="robbyrussell"/c\ZSH_THEME="powerlevel10k/powerlevel10k"' ~/.zshrc
+    #ZSH OH-MY-ZSH
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 # INSTALL NODEJS Y NPM
-sudo pacman -Syy nodejs npm
-sudo pacman -Syy nodejs-express
+sudo pacman --noconfirm -Syy nodejs npm
+sudo pacman --noconfirm -Syy nodejs-express
 
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
